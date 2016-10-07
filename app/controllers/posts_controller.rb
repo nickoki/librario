3b3a0237c1aa90dec03d80ab143ac5626248053f
Nick Oki <nick.oki@live.com>
2016-10-07 15:16:07 -0400

    Init PostsController, main stylesheet file

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  # Controller#Action index
+  #
+  def index
+    
+  end
+
+  # Controller#Action new
+  #
+
+
+  # Controller#Action create
+  #
+
+
+  # Controller#Action show
+  #
+
+
+  # Controller#Action edit
+  #
+
+
+  # Controller#Action update
+  #
+
+  # Controller#Action delete
+  #
+end
