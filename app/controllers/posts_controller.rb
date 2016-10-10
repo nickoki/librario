7195f5b048b425536923d5f11a941d47e9129b1b
Nick Oki <nick.oki@live.com>
2016-10-10 10:47:20 -0400

    Added delete controller route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,11 @@ class PostsController < ApplicationController
 
   # Controller#Action delete
   #
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
   private
   def post_params
