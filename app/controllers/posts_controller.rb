de804d73080f5696dee97e8557c3378428fbc528
Nick Oki <nick.oki@live.com>
2016-10-10 10:32:41 -0400

    Added show, new, and create controller routes and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,27 @@ class PostsController < ApplicationController
   # Controller#Action index
   #
   def index
-    
+    @posts = Post.all
   end
 
   # Controller#Action new
   #
-
+  def new
+    @post = Post.new
+  end
 
   # Controller#Action create
   #
-
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
+  end
 
   # Controller#Action show
   #
-
+  def show
+    @post = Post.find(params[:id])
+  end
 
   # Controller#Action edit
   #
@@ -27,4 +34,9 @@ class PostsController < ApplicationController
 
   # Controller#Action delete
   #
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :link, :description, :link_content_date, :user_id, :created_at, :update_at)
+  end
 end
